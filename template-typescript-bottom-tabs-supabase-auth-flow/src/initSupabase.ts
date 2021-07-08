7496347fad76d5b1463e4d66c7488766209f96c3
Joseph Moshood <gladstargtschool6@gmail.com>
2021-07-08 10:58:23 +0100

    Update initSupabase.ts

--- template-typescript-bottom-tabs-supabase-auth-flow/src/initSupabase.ts
+++ template-typescript-bottom-tabs-supabase-auth-flow/src/initSupabase.ts
@@ -2,6 +2,6 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 
 // Better put your these secret keys in .env file
-export const supabase = createClient("supabaseUrl", "supabaseKey", {
+export const supabase = createClient("https://imdvvfcbtrfewysxgrnr.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNDMzNzg3MSwiZXhwIjoxOTI5OTEzODcxfQ.O7Ck0RFbXxsJ9BpxdAuey5O_MJcC_lHM06xDYKTbWxg", {
   localStorage: AsyncStorage as any,
 });
